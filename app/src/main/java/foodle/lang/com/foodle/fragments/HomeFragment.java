bc2e0b56a22b9069c0fa59ed58129396b13f76cf
lang970801 <1358236089@qq.com>
2017-07-11 22:33:00 +0800

    我的界面一完善三

--- app/src/main/java/foodle/lang/com/foodle/fragments/HomeFragment.java
+++ app/src/main/java/foodle/lang/com/foodle/fragments/HomeFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +23,7 @@ public class HomeFragment extends Fragment {
     private GridView gv;
     private ImageView img;
     private static final int VIEWPAGER = 0;
-    private List<Map<String,Object>> list;
+    private ArrayList<Map<String,Object>> list;
     private SimpleAdapter simpleAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,14 +38,20 @@ public class HomeFragment extends Fragment {
         home_lv=view.findViewById(R.id.home_lv);
         img=view.findViewById(R.id.home_img);
         gv=view.findViewById(R.id.gv_layout);
+        simpleAdapter=new SimpleAdapter(view.getContext(),list,R.layout.item_gridview_layout,new String[]{"IMG","NAME"},new int[]{R.id.gv_img,R.id.gv_text});
+        gv.setAdapter(simpleAdapter);
     }
 
     private void ininData() {
+        list=new ArrayList<Map<String,Object>>();
         String[] imgNanme = getResources().getStringArray(R.array.imgName);
         int[] img={R.mipmap.ic_dd,R.mipmap.ic_wm, R.mipmap.ic_xckc,R.mipmap.ic_gac, R.mipmap.ic_hg,R.mipmap.ic_zzc_pp,
                 R.mipmap.ic_srdg,R.mipmap.ic_tdyp,};
         for (int i=0;i<imgNanme.length;i++){
             Map<String,Object> map=new HashMap<String,Object>();
+            map.put("IMG",img[i]);
+            map.put("NAME",imgNanme[i]);
+             list.add(map);
         }
     }
 
