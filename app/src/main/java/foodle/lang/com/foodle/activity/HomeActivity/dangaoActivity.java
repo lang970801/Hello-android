d70fbf19aa3c240d484cc26b664d8ad071aa8b05
Administrator <git@github.com:lang970801/Foodie.git>
2017-07-13 12:53:37 +0800

    安德森

---
+++ app/src/main/java/foodle/lang/com/foodle/activity/HomeActivity/dangaoActivity.java
@@ -0,0 +1,15 @@
+package foodle.lang.com.foodle.activity.HomeActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/7/13.
+ */
+public class dangaoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
