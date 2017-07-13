1d1aa834a0f223271864d5c4fc8a3e72474d3f24
GuangXi XuWenQiang <1262643466@qq.com>
2017-07-13 10:50:29 +0800

    PopupWindow

---
+++ app/src/main/java/foodle/lang/com/foodle/utils/PopupWindow.java
@@ -0,0 +1,35 @@
+package foodle.lang.com.foodle.utils;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.View;
+
+import foodle.lang.com.foodle.R;
+
+/**
+ * Created by Administrator on 2017/7/13/013.
+ */
+
+public class PopupWindow{
+   public void PopupWindow(){
+
+//       final View popupView=getActivity().getLayoutInflater().inflate(R.layout.merchan_filter_column,null);
+//       popupWindow=new android.widget.PopupWindow(popupView, ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT,true);
+//       popupWindow.setTouchable(true);
+//       popupWindow.setOutsideTouchable(true);
+//       popupWindow.setBackgroundDrawable(new BitmapDrawable(Resources.getSystem(),(Bitmap)null));
+//       mButton=view.findViewById(R.id.merchan_bt1);
+//       mButton.setOnClickListener(new View.OnClickListener() {
+//           public void onClick(View view) {
+//               popupWindow.showAsDropDown(view);
+//           }
+//       });
+   }
+
+
+
+
+}
